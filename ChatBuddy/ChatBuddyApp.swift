91c66648546459a27671938bd7afc798fc5f8d88
pdheisenberg <parthdhawan28@gmail.com>
2023-04-11 16:43:23 +0800

    Initial Commit

---
+++ ChatBuddy/ChatBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBuddyApp.swift
+//  ChatBuddy
+//
+//  Created by Parth Dhawan on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatBuddy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatBuddy
+//
+//  Created by Parth Dhawan on 11/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
